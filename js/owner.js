2052ae1bdf54e9ac54d51354c90a1c5a089b3474
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-06-07 13:02:13 +0100

    Added owner.

---
+++ js/owner.js
@@ -0,0 +1,17 @@
+class Owner {
+firstname;
+lastname;
+address;
+city;
+telephone;
+pets;
+
+    constructor(firstname, surname, address, city, telephone) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone
+    }
+    
+}
